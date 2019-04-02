9728e102125e9db31267bdaf03af194aab96262c
jiaomengfei <jiaomengfei@yyhudong.com>
2019-04-02 11:06:56 +0800

    追书Mod导航小说列表加载更多

--- app/ZhuiShu/src/main/java/com/zhuishu/TestActivity.java
+++ app/ZhuiShu/src/main/java/com/zhuishu/TestActivity.java
@@ -19,13 +19,14 @@ public class TestActivity extends Activity {
     final Zs qd = new Zs();
     new Thread(new Runnable() {
       @Override public void run() {
-        JSONArray result = qd.getNovelInfoByAuthor("天蚕土豆");
+        //JSONArray result = qd.getNovelInfoByAuthor("天蚕土豆");
         //JSONArray result = qd.getNavigateItem("http://www.zhuishushenqi.com/ranking/5645482405b052fe70aeb1b5?type=female");
         //JSONArray result = qd.getNavigateList();
         //JSONArray result = qd.getRecommendNovels();
         //String chapterContent = qd.getChapterContent(
         //    "http://www.zhuishushenqi.com/book/50864deb9dacd30e3a00001d/1.html").toString();
         //String novelInfoByNovelName = qd.getNovelInfoByName("武动乾坤").toString();
+        JSONArray result = qd.getNavigateItem("http://www.zhuishushenqi.com/ranking/54d42d92321052167dfb75e3?type=male",1);
         Log.e("<Novel>", "-->> : " + result);
       }
     }).start();
