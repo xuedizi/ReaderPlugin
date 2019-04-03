6a7a773ac168552a9c5c8299025255c9f5543a7a
xichao <xichao@yyhudong.com>
2019-04-03 11:17:45 +0800

    图片爬虫基类

--- app/picture/PictureSupport/src/main/java/com/support/picture/IPictureSupport.java
+++ app/picture/PictureSupport/src/main/java/com/support/picture/IPictureSupport.java
@@ -1,5 +1,7 @@
 package com.support.picture;
 
+import org.json.JSONArray;
+
 /**
  * TODO
  *
@@ -8,4 +10,19 @@ package com.support.picture;
  * @created 2019/4/2
  */
 public interface IPictureSupport {
+
+  /**
+   * 获取推荐的图集
+   */
+  JSONArray getRecommendPictures();
+
+  /**
+   * 获取图集站点导航栏内容
+   */
+  JSONArray getNavigateList();
+
+  /**
+   * 通过页数获取导航列表
+   */
+  JSONArray getNavigateItem(String url, int page);
 }
